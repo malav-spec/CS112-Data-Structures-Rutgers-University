f70a2e2fdbd73b42c99863265b9868956b90a8dc
Malav Doshi <56772095+malav-spec@users.noreply.github.com>
2020-09-23 18:59:40 +0530

    Add files via upload

---
+++ MST/Driver.java
@@ -0,0 +1,34 @@
+
+package app;
+
+import structures.Arc;
+import structures.Graph;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Class used for testing the MST
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        Graph graph = null;
+        try {
+            graph = new Graph("graph1.txt");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        //
+        PartialTreeList partialTreeList = PartialTreeList.initialize(graph);
+        //
+        ArrayList<Arc> arcArrayList = PartialTreeList.execute(partialTreeList);
+        //
+
+        for (int i = 0; i < arcArrayList.size(); i++) {
+            Arc anArcArrayList = arcArrayList.get(i);
+            System.out.println(anArcArrayList);
+        }
+    }
+}
