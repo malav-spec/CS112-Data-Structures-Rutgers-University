97ed1c16a518ee7f24617942f050f4661202976e
Malav Doshi <56772095+malav-spec@users.noreply.github.com>
2020-10-02 12:04:40 +0530

    Update Tree.java

--- DOM Tree/Tree.java
+++ DOM Tree/Tree.java
@@ -37,6 +37,60 @@ public class Tree {
 	 */
 	public void build() {
 		/** COMPLETE THIS METHOD **/
+		if(!sc.hasNextLine())
+		{
+			return;
+		}
+		
+		root=new TagNode(tagName(sc.nextLine()),null,null);//The first node "html" of the tree
+		
+		Stack<TagNode> tags=new Stack<TagNode>();
+		tags.push(root);
+		String name="";
+		while(sc.hasNextLine())//iterates the whole HTML document
+		{
+		 	name=sc.nextLine();
+		 	boolean isTag=false;
+		 	
+		 	if(name.charAt(0)=='<')
+		 	{
+		 		name=tagName(name);
+		 		isTag=true;
+		 	}
+		 	
+		 	if(isTag && (name.charAt(0)=='/'))
+			{
+		 		tags.pop();	
+		    }
+		 	else
+		 	{
+		 		TagNode element=new TagNode(name,null,null);
+		 		if(tags.peek().firstChild==null)
+		 		{
+		 			tags.peek().firstChild=element;
+		 		}
+		 		else 
+		 		{
+		 			TagNode child=tags.peek().firstChild;
+		 			
+		 			while(child.sibling!=null)
+		 			{
+		 				child=child.sibling;
+		 			}
+		 		     
+		 			child.sibling=element;
+		 		}
+		 		
+		 		if(isTag)
+		 		{
+		 			tags.push(element);
+		 		}
+		 		
+		 	}
+		 			 			
+		}
+		
+		
 	}
 	
 	/**
@@ -46,7 +100,33 @@ public class Tree {
 	 * @param newTag Replacement tag
 	 */
 	public void replaceTag(String oldTag, String newTag) {
+		
 		/** COMPLETE THIS METHOD **/
+		if(oldTag==null || newTag==null || root==null)
+		{
+			return;
+		}
+		else
+		{
+			this.ReplaceTag(root.firstChild,oldTag,newTag);
+		}
+			
+		
+	}
+	
+	private void ReplaceTag(TagNode Node, String Old, String New )
+	{
+		if(Node==null)
+		{
+			return;
+		}
+		else if(Node.tag.compareTo(Old)==0)
+		{
+			Node.tag=New;
+		}
+		
+		this.ReplaceTag(Node.firstChild, Old, New);//Inorder traversals
+		this.ReplaceTag(Node.sibling, Old, New); 
 	}
 	
 	/**
@@ -57,6 +137,63 @@ public class Tree {
 	 */
 	public void boldRow(int row) {
 		/** COMPLETE THIS METHOD **/
+		
+		TagNode current=new TagNode(null,null,null);
+		
+		TagNode i;
+		
+		current=makeBold(root);
+		
+		if(current==null)
+		{
+			System.out.println("Table oes not exist");
+			return;
+		}
+		
+		current=current.firstChild;
+		
+		int j=1;//traverse through rows
+		while(j<row)
+		{
+			current=current.sibling;
+			j=j+1;
+		}
+		
+		i=current.firstChild;
+		while(i!=null)
+		{
+			i.firstChild=new TagNode("b",i.firstChild,null);
+			i=i.sibling;
+		}
+		
+	}
+	
+	private TagNode makeBold(TagNode temp)
+	{
+		if(temp==null)
+		{
+			return null;
+		}
+		
+		TagNode temp1=null;
+		String x=temp.tag;
+		
+		if(x.compareTo("table")==0)
+		{
+			temp1=temp;
+			return temp1;
+		}
+		
+		if(temp1==null)
+		{
+			temp1=makeBold(temp.sibling);
+		}
+		
+		if(temp1==null)
+		{
+			temp1=makeBold(temp.firstChild);
+		}
+		return temp1;
 	}
 	
 	/**
@@ -68,8 +205,91 @@ public class Tree {
 	 */
 	public void removeTag(String tag) {
 		/** COMPLETE THIS METHOD **/
+		if((root==null) || (tag==null))
+		{
+			return;
+		}
+		else 
+		{
+			while(hasTag(tag,root))
+			{
+				remover(tag,root,root.firstChild);
+			}
+		}
+		
 	}
 	
+	private boolean hasTag(String tag, TagNode temp)
+	{
+		if(temp==null)
+		{
+			return false;
+		}
+		else if(temp.tag.equals(tag))
+		{
+			return true;
+		}
+		
+		boolean tbd=(hasTag(tag,temp.firstChild) || hasTag(tag,temp.sibling));
+		
+		return tbd;
+	}
+	
+	private void remover(String tag, TagNode head, TagNode temp)
+	{
+		if(temp== null || head==null)
+		{
+			return;
+		}
+		else if(temp.tag.equals(tag))
+		{
+			if(tag.equals("ol") || tag.equals("ul"))
+			{
+				TagNode child=temp.firstChild;
+				
+				while(child!=null)
+				{
+					if(child.tag.equals("li"))
+					{
+						child.tag="p";
+					}
+					
+					child=child.sibling;
+				}
+			}
+			
+			if(head.firstChild==temp)
+			{
+				head.firstChild=temp.firstChild;
+				TagNode child=temp.firstChild;
+				
+				while(child.sibling!=null)
+				{
+					child=child.sibling;
+				}
+				
+				child.sibling=temp.sibling;
+			}
+			else if(head.sibling==temp)
+			{
+				TagNode child=temp.firstChild;
+				
+				while(child.sibling!=null)
+				{
+					child=child.sibling;
+				}
+				
+				child.sibling=temp.sibling;
+				head.sibling=temp.firstChild;
+			}
+			return;
+		}
+		
+		head=temp;
+		remover(tag,head,temp.firstChild);
+		remover(tag,head,temp.sibling);
+
+	}
 	/**
 	 * Adds a tag around all occurrences of a word in the DOM tree.
 	 * 
@@ -78,7 +298,269 @@ public class Tree {
 	 */
 	public void addTag(String word, String tag) {
 		/** COMPLETE THIS METHOD **/
+		
+		tagAdder(root, null,word,tag);
 	}
+	
+	private void tagAdder(TagNode curr, TagNode prev, String Word, String Tag)
+	{
+		if(curr==null)
+		{
+			return;
+		}
+		
+		if(prev!=null && prev.tag==Tag)
+		{
+			return;
+		}
+		
+		if(Tag.equals("html") || Tag.equals("body") || Tag.equals("p") || Tag.equals("em") || Tag.equals("tr") || Tag.equals("td") || Tag.equals("ol") || Tag.equals("ul") || Tag.equals("b") || Tag.equals("table") || Tag.equals("li")) 
+
+		{
+			if(curr.tag.equals("html") || curr.tag.equals("body") || curr.tag.equals("p") || curr.tag.equals("em") || curr.tag.equals("tr") || curr.tag.equals("td") || curr.tag.equals("ol") || curr.tag.equals("ul") || curr.tag.equals("b") || curr.tag.equals("table") || curr.tag.equals("li"))
+			{
+				
+			}
+			else
+			{
+				String temp[]=curr.tag.split(" ");
+				
+				int i=temp.length;
+				int l=0;
+				
+				String previous,target,after;
+				
+				TagNode x=new TagNode(Tag,null,null);
+				
+				if(l==1) 
+				{
+					int j;
+					for(j=0;j<l;j=j+1) 
+					{
+						if(Compare(temp[i], Word, null))
+						{
+							if(prev.firstChild==curr)
+							{
+								if(curr.sibling!=null)
+								{
+									prev.firstChild=x;
+									x.firstChild=curr;
+									x.sibling=curr.sibling;
+									curr.sibling=null;
+								}
+								else
+								{
+									prev.firstChild=x;
+									x.firstChild=curr;
+								}
+							}
+							
+							if(prev.sibling==curr)
+							{
+								if(curr.sibling!=null)
+								{
+									prev.sibling=x;
+									x.firstChild=curr;
+									x.sibling=curr.sibling;
+									curr.sibling=null;
+								}
+								else
+								{
+									prev.sibling=x;
+									x.firstChild=curr;
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					boolean B=true;
+					boolean A=true;
+					boolean T=true;
+					
+					TagNode head=null;
+					TagNode last=null;
+					
+					while(T==true) 
+					{
+						TagNode temp1=new TagNode(null,null,null);
+						TagNode temp2=new TagNode(null,null,null);
+						TagNode temp3=new TagNode(null,null,null);
+						
+						previous="";
+						target="";
+						after="";
+						int c;
+						for(c=0;c<l && (A==true);c++) 
+						{
+							if(Compare(temp[c], Word,null))
+							{
+								B=false;
+								A=false;
+								
+								String Q=temp[c];
+								target=Q;
+								
+								temp2.tag=target;
+								
+								int I=l-1;
+								
+								if(c!=I)
+								{
+									int p;
+									for(p=c+1;p<l;p++)
+									{
+										after=after+temp[p]+" ";
+									}
+									
+									temp3.tag=after;
+								}
+							}
+							else if(B==true)
+							{
+								previous=previous+temp[c]+" ";
+								temp1.tag=previous;
+							}
+						}
+						
+						if(A==true)
+						{
+							if(prev.firstChild==curr)
+							{
+								prev.firstChild=temp1;
+							}
+							
+							if(prev.sibling==curr)
+							{
+								prev.sibling=temp1;
+							}
+							
+							break;
+						}
+						
+						if(temp1.tag!=null && temp2.tag!=null && temp3.tag!=null)
+						{
+							temp1.sibling=x;
+							x.firstChild=temp2;
+							x.sibling=temp3;
+						}
+						else if(temp1.tag!=null && temp2.tag!=null)
+						{
+							temp1.sibling=x;
+							x.firstChild=temp2;
+						}
+						else if(temp3.tag!=null)
+						{
+							x.firstChild=temp2;
+							x.sibling=temp3;
+						}
+						
+						if(head==null && temp1.tag!=null)
+						{
+							head=temp1;
+						}
+						else if(head==null && temp1.tag==null)
+						{
+							x.firstChild=temp2;
+							head=x;
+						}
+						else
+						{
+							last=temp2;
+						}
+						
+						if(temp3.tag!=null)
+						{
+							T=true;
+							
+							temp=temp3.tag.split(" ");
+							
+							l=temp.length;
+							
+							if(head==null && temp1.tag!=null)
+							{
+								head=temp1;
+							}
+							else if(head==null && temp1.tag==null)
+							{
+								x.firstChild=temp2;
+								head=x;
+							}
+							else 
+							{
+								last=temp3;
+							}
+						}
+						else
+						{
+							T=false;
+						}
+					}
+					if(prev.firstChild==curr)
+					{
+						prev.firstChild=head;
+					}
+					else if(prev.sibling==curr)
+					{
+						prev.sibling=head;
+					}
+					
+					if(curr.sibling!=null)
+					{
+						last.sibling=curr.sibling;
+						curr.sibling=null;
+					}
+					
+				}
+			}
+			tagAdder(curr.firstChild,curr,Word,Tag);
+			tagAdder(curr.sibling,curr,Word,Tag);
+						
+		}
+	}
+	
+	private boolean Compare(String curr, String target, TagNode x)
+	{
+		String d=curr.toLowerCase();
+		String e=target.toLowerCase();
+		
+		int L=curr.length();
+		char chr=curr.charAt(L-1);
+		
+		String s1=d;
+		String s2=e;
+		
+		if(s1.equals(s2))
+		{
+			return true;
+		}
+		
+		char last=chr;
+		String sub=s1.substring(0,curr.length()-1);
+		
+		if(Character.isLetter(last))
+		{
+			return false;
+		}
+		else if(s2.equals(sub))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	//Helper Methods start
+	
+	private static String tagName(String name)
+	{
+		return name.substring(1,name.length()-1);
+	}
+	
+	//Helper method end
 	
 	/**
 	 * Gets the HTML represented by this DOM tree. The returned string includes
